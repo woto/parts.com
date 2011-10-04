c28ece3aeddd3fb7b67279884755e85aa3b25cae
Ruslan Kornev <oganer@gmail.com>
2011-10-04 20:24:51 +0200

    Локализация, валидация Proxy

--- server/app/models/proxy.rb
+++ server/app/models/proxy.rb
@@ -1,3 +1,10 @@
 class Proxy < ActiveRecord::Base
-  #TODO добавить валидацию как только убежусь что происходит ошибка при добавлении записи с пустым значением порт
+  @ip_regex = /^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/
+  validates :ip, :presence => true, :uniqueness => true, :format => {:with => @ip_regex}
+  validates :good, :presence => true, :numericality => {:only_integer => true}
+  validates :port, :presence => true, :numericality => {:only_integer => true}
+  validates :speed, :numericality => {:only_integer => true}, :allow_blank => true
+  validates :connection_time, :allow_blank => true, :numericality => {:only_integer => true}
+  validates :protocol, :inclusion => {:in => ["HTTP", "HTTPS"]}, :allow_blank => true
+
 end
